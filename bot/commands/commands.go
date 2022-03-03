268763526dcfe82f45667c5fc877d744b7506e20
We-xlr <86886695+We-xlr@users.noreply.github.com>
2022-03-03 10:46:21 -0800

    hm

--- bot/commands/commands.go
+++ bot/commands/commands.go
@@ -22,7 +22,7 @@ func contains(s []int, e int) bool {
 
 
 func Start(b *gotgbot.Bot, ext *ext.Context) error {
-	b.SendMessage(ext.Message.Chat.Id, "Hello There\nI'mSylviorus Bot", &gotgbot.SendMessageOpts{
+	b.SendMessage(ext.Message.Chat.Id, "Hello There\nI'Myself GbanWatch System Bot", &gotgbot.SendMessageOpts{
 		ParseMode: "markdown",
 	})
 
@@ -81,16 +81,16 @@ func UnBanCmd(b *gotgbot.Bot, ext *ext.Context) error {
 
 func Status(b *gotgbot.Bot , ext *ext.Context) error {
 	caption_text := fmt.Sprintf(`
-	<b>Welcome Master %v</b>
+	<b>Welcome User %v</b>
 
-	<i>The Vampire Of BlueMoon</i>
+	<i>You are a valid user!</i>
 	
-	<b>Status</b> : Clan Of BlueMoon
+	<b>Status</b> : Verified User
 	
 	<b>Use me to slay the CrimsonMoon</b>
 ` , ext.Message.From.FirstName)
 
-	devs := []int{5014715207, 825664681, 1091139479, 2076788242, 1719660492, 1700123830, 1707848873, 2079472115, 2107137268, 870471128, 1742378072, 2108196300, 1802324609, 645739169, 1483971607, 1757774449, 1306525471, 769830161, 5086015489, 5244167973}
+	devs := []int{5001573230}
 	if contains(devs , int(ext.Message.From.Id)) {
 		b.SendPhoto(ext.Message.Chat.Id , "https://telegra.ph/file/6369995ee354ab21c6f06.jpg" , &gotgbot.SendPhotoOpts{
 			Caption: caption_text,
